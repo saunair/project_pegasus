c475f7e4fd1404dc056411dfdb7715c335eea1a4
Saurabh Nair <saurabh@vicarious.com>
2020-12-26 16:08:05 -0800

    power subsets

---
+++ leetcode/power_subsets.py
@@ -0,0 +1,21 @@
+"""Power Set Power set P(S) of a set S is the set of all subsets of S. For example S = {a, b, c} then P(s) = {{}, {a}, {b}, {c}, {a,b}, {a, c}, {b, c}, {a, b, c}}.
+If S has n elements in it then P(s) will have 2^n elements"""
+
+
+
+def generate_power_sets(sample_list):
+    num_of_combinations = pow(2, len(sample_list))
+    all_sets = []
+    for element_number in range(num_of_combinations):
+        set_for_number = []
+        for binary_num in range(len(sample_list)):
+            if (1 << binary_num) & element_number:
+                set_for_number.append(sample_list[binary_num])
+        all_sets.append(set_for_number)
+    return all_sets
+
+
+
+if __name__ == "__main__":
+    a = ["a", "b", "c"]
+    b = generate_power_sets(a) 
