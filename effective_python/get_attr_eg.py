52660e7ef255b1934de0fe479a544399fa290638
Saurabh Nair <saurabh@vicarious.com>
2021-01-04 15:03:16 -0800

    get set updates

--- effective_python/get_attr_eg.py
+++ effective_python/get_attr_eg.py
@@ -1,8 +1,15 @@
+# Example for weakref & __get__ + __set__ usages.
+
 import weakref
 
 
+# Here the Grade acts like a property, but that can be resued as multiple properties to a class using this.
+# This way a validator in the __set__ method doesn't have to be re-written.
 class Grade:
     def __init__(self):
+        # Wealref for the case the key is removed, we need this dictionary to delete the entry automatically, 
+        # hence giving the interpretor more memory by removing the value associated to it.
+        # Using this helps the garbage collector to kick in sooner, than just the end of the program.
         self._values = weakref.WeakKeyDictionary()
 
     def __get__(self, instance, instance_type):
@@ -17,25 +24,26 @@ class Grade:
 
 
 # The get / set methods work i.e. the validator works only on class attributes
+# Hence we cannot construct this in initialization of Exam(). We have to set the properties 
+# like `math_grade` after Exam() is initialized. Till then the value of the property is None.
 class Exam:
     math_grade = Grade()
     writing_grade = Grade()
     science_grade = Grade()
     geo_grade = Grade()
-    def __init__(self, math, writing, geo, science):
-        self.math_grade = math
-        self.writing_grade = writing
-        self.science_grade = science
-        self.geo_grade = geo 
 
 
 if __name__ == "__main__":
-    example_exam = Exam(
-        math=10,
-        writing=20,
-        geo=30,
-        science=50
-    )
-    example_exam.science_grade = 10
+    example_exam = Exam()
     # None of the validators work for the self.attributes created after the 
-    example_exam.new_subject_grade = -200
+    example_exam.math_grade = 20
+    
+    # Make sure the instances have their own values.
+    new_exam = Exam()
+    assert new_exam.science_grade is None, "We haven't assigned a value yet"
+    try:
+        new_exam.science_grade = -200
+        assert False, "Didn't raise an error for a value lower"
+    except ValueError:
+        print("Works as expected")
+        pass
