ff3df003b00fb4c2e2f008ac96ff0a0c89d1b065
Saurabh Nair <saurabh@vicarious.com>
2021-01-17 18:20:36 -0800

    adding binary tree

--- leetcode/binary_tree.py
+++ leetcode/binary_tree.py
@@ -2,10 +2,53 @@
 
 
 class Node:
-    def __init__(self, data, left, right):
+    def __init__(self, data=None, left=None, right=None):
         self.__data = data
         self.__left = left
         self.__right = right 
+    
+    @classmethod
+    def make_tree_from_sorted_list(cls, sorted_list):
+        """Make tree recursively from the sorted-list."""
+        if len(sorted_list) == 1:
+            return cls(data=sorted_list[0])
+
+        elif len(sorted_list) == 0:
+            return cls(data=None)
+
+        mid_val = int(len(sorted_list) / 2)
+        node = cls(data=sorted_list[mid_val])
+        node.left = cls.make_tree_from_sorted_list(sorted_list[:mid_val])
+        node.right = cls.make_tree_from_sorted_list(sorted_list[mid_val + 1:])
+        return node
+    
+    def insert(self, data):
+        assert isinstance(data, float), f"Data to be inserted has to be a {float} not {type(data)}"
+        if self.data is None:
+            self.data = data
+            return
+
+        if data > self.data:
+            if self.right is not None:
+                self.right.insert(data)
+            else:
+                self.right = Node(data=data)
+            return
+
+        assert data <= self.data
+        if self.left is not None:
+            self.left.insert(data)
+            return
+
+        self.left = Node(data=data)
+        return 
+
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print(f" {self.data}")
+        if self.right:
+            self.right.print_tree()
 
     @property
     def data(self):
@@ -34,18 +77,18 @@ class Node:
         assert isinstance(data, float) or isinstance(data, None)
         self.__data = data
 
+    @property
+    def max_depth(self):
+        """Assumig a balanced tree here"""
+        if self.data is None:
+            return 0
 
-def get_max_depth(node):
-    """Assumig a balanced tree here"""
-    if node.data is None:
-        return 0
+        if self.left is not None and self.right is not None:
+            child_depth = max(self.left.max_depth, self.right.max_depth)
+            return child_depth + 1
 
-    if node.left is not None and node.right is not None:
-        child_depth = max(get_max_depth(node.left), get_max_depth(node.right))
-        return child_depth + 1
-
-    # left or right is not None, just one of the lengths left.
-    return 1
+        # left or right is not None, just one of the lengths left.
+        return 1
 
 
 # Minor test script
@@ -53,7 +96,7 @@ left_node = Node(1, None, None)
 right_node = Node(3, None, None)
 root_node = Node(2, left=left_node, right=right_node)
 
-assert get_max_depth(root_node) == 2
+assert root_node.max_depth == 2
 
 
 """ Current tree:
@@ -73,4 +116,12 @@ left_node.right = Node(4, None, None)
 
 right_node.left = Node(6, None, None)
 
-assert get_max_depth(root_node) == 3
+root_node.print_tree()
+assert root_node.max_depth == 3
+
+example_sorted_list = [float(num) for num in range(30)]
+
+root_node = Node.make_tree_from_sorted_list(example_sorted_list)
+import IPython; IPython.embed()
+print(root_node.print_tree())
+
