116e9c6034f2d18a207dea0ee33b642af1d3058c
Saurabh Nair <saurabh@vicarious.com>
2021-01-17 17:34:20 -0800

    adding binary tree

--- leetcode/binary_tree.py
+++ leetcode/binary_tree.py
@@ -1,3 +1,5 @@
+# Maximum number of nodes at level "l" is 2^l. Level of the root is 0.
+
 
 class Node:
     def __init__(self, data, left, right):
@@ -18,22 +20,57 @@ class Node:
         return self.__left
     
     @right.setter
-    def set_right(self, node):
-        assert isinstance(Node, node) or isinstance(Node, None)
+    def right(self, node):
+        assert isinstance(node, Node) or isinstance(node, None)
         self.__right = node
     
     @left.setter
-    def set_left(self, node):
-        assert isinstance(Node, node) or isinstance(Node, None)
+    def left(self, node):
+        assert isinstance(node, Node) or isinstance(node, None)
         self.__left = node
     
     @data.setter
-    def set_data(self, data):
-        assert isinstance(data, float)
+    def data(self, data):
+        assert isinstance(data, float) or isinstance(data, None)
         self.__data = data
+
+
+def get_max_depth(node):
+    """Assumig a balanced tree here"""
+    if node.data is None:
+        return 0
+
+    if node.left is not None and node.right is not None:
+        child_depth = max(get_max_depth(node.left), get_max_depth(node.right))
+        return child_depth + 1
+
+    # left or right is not None, just one of the lengths left.
+    return 1
 
 
 # Minor test script
 left_node = Node(1, None, None)
 right_node = Node(3, None, None)
 root_node = Node(2, left=left_node, right=right_node)
+
+assert get_max_depth(root_node) == 2
+
+
+""" Current tree:
+
+      5
+    /   \
+   3     7
+  / \   / \
+ 1   4 6  none
+  
+"""
+left_node = Node(3, None, None)
+right_node = Node(7, None, None)
+root_node = Node(5, left=left_node, right=right_node)
+left_node.left = Node(1, None, None)
+left_node.right = Node(4, None, None)
+
+right_node.left = Node(6, None, None)
+
+assert get_max_depth(root_node) == 3
