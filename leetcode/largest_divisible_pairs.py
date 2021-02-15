1cdb653c7f8a3d2e69d6a83929b53c3c29fc1454
Saurabh Nair <saurabh@vicarious.com>
2021-02-15 00:50:07 -0800

    largest divisible clear

--- leetcode/largest_divisible_pairs.py
+++ leetcode/largest_divisible_pairs.py
@@ -2,7 +2,7 @@
 
 
 
-def get_largest(num_list):
+def get_largest_divisible_pair(num_list):
     # If the list doesn't have anything to compute on, return the same.
     if len(num_list) < 2:
         return num_list
@@ -14,7 +14,6 @@ def get_largest(num_list):
     # A list to keep count of the previous element in the list.
     number_of_successors = len(num_list) * [0]
     
-    biggest_leaf = 0
     #number id would now go across all the lists here.
     for number_id in range(len(num_list)):
         for lower_number_id in range(number_id):
@@ -25,22 +24,26 @@ def get_largest(num_list):
                     # Swap the numbers / link to point to this new successor.
                     previous_element[number_id] = lower_number_id
                     number_of_successors[number_id] = number_of_successors[lower_number_id] + 1
-        # Going to the next (larger) number, check whether the current has a higher successor count .
 
-        if number_of_successors[number_id] > number_of_successors[biggest_leaf]:
-            biggest_leaf = number_id
-    
+    # take an argmax to find the element with the highest succesors.
+    biggest_leaf = number_of_successors.index(max(number_of_successors))
+
+    # Now just iterate through the successors to get the set
     largest_divisor_subset = []
     while True:
         current_number = num_list[biggest_leaf]
         largest_divisor_subset.append(current_number)
-        if number_of_successors[biggest_leaf] == 0:
-            return largest_divisor_subset
-        biggest_leaf = previous_element[biggest_leaf]
+        if number_of_successors[biggest_leaf] != 0:
+            biggest_leaf = previous_element[biggest_leaf]
+            # The list is done, now just return
+            continue
+        # If hit zero successors already, we are at the atomic element of the set.
+        break
 
+    return largest_divisor_subset
 
 if __name__ == "__main__":
-    assert get_largest([10, 5, 3, 15, 20]) == [20, 10, 5]
-    assert get_largest([18, 1, 3, 6, 13, 17]) == [18, 6, 3, 1]
-    assert get_largest([2]) == [2]
-    assert get_largest([]) == []
+    assert get_largest_divisible_pair([10, 5, 3, 15, 20]) == [20, 10, 5]
+    assert get_largest_divisible_pair([18, 1, 3, 6, 13, 17]) == [18, 6, 3, 1]
+    assert get_largest_divisible_pair([2]) == [2]
+    assert get_largest_divisible_pair([]) == []
