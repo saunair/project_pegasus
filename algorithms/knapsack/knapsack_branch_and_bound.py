aacd8808cfb928d8d52be73ca17561fcc075b3d0
Saurabh Nair <saurabh@vicarious.com>
2021-08-19 20:40:27 -0700

    adding the first branch and bound

---
+++ algorithms/knapsack/knapsack_branch_and_bound.py
@@ -0,0 +1,123 @@
+"""Knapsack solution using the branch and bound method."""
+
+class Item:
+    """Representation of one item from the total set of objects that can be chosen."""
+    def __init__(self, weight: int, value: int):
+        # Todo: Make this a dataclass.
+        self.__weight = weight
+        self.__value = value
+
+    def __repr__(self):
+        return f"weight: {self.weight}, value:{self.value}, ratio:{self.ratio}"
+    
+    @property
+    def value(self):
+        return self.__value
+    
+    @property
+    def weight(self):
+        return self.__weight
+    
+    #@weight.setter
+    #def weight(self, w):
+    #    self.__weight = w
+
+    #@value.setter
+    #def value(self, v):
+    #    self.__value = v
+
+    @property
+    def ratio(self):
+        return self.value / self.weight
+    
+    def __ge__(self, other):
+        if self.ratio >= other.ratio:
+            return True
+        if self.ratio < other.ratio:
+            return False
+
+
+class Node: 
+    """Node of the branch and bound tree."""
+    def __init__(self, level: int = None, profit: int = None, bound: float = None, weight: float = None):
+        # TODO: make this a Dataclass.
+        self.level = level
+        self.profit = profit
+        self.bound = bound
+        self.weight = weight
+
+
+def bound(node_u: Node, total_knapsack_weight: int, list_of_items: list) -> int:
+    # If the node's weight exceeds the capacity, return a zero value as (an early exit) solution.
+    if node_u.weight >= total_knapsack_weight:
+        return 0
+
+    profit_bound = node_u.profit
+    total_weight = node_u.weight
+    item_number = node_u.level + 1
+
+    while item_number < len(list_of_items) and total_weight + list_of_items[item_number] <= total_knapsack_weight:
+        total_weight += list_of_items[item_number].weight
+        profit_bound += list_of_items[item_number].value
+        item_number += 1
+
+    if item_number < n:
+        profit_bound += (total_knapsack_weight - total_weight) * list_of_items[item_number].ratio
+
+    return profit_bound
+
+
+def knapsack_bb(total_knapsack_weight: int, item_value_pairs: list):
+    """Knapsack using greedy branch and bound."""
+    def _item_ratio(item):
+        return item.ratio
+
+    item_value_pairs = [Item(weight=weight, value=value) for weight, value in item_value_pairs]
+    list_of_items.sort(key=_item_ratio, reverse=True)
+
+    current_queue = []
+    u, v = Node(level=-1, profit=0, weight=0), Node(level=-1, profit=0, weight=0)
+    max_profit = 0
+
+    while len(current_queue) > 0:
+        u = current_queue.pop(-1)
+        if u.level == -1:
+            v.level = 0
+
+        if u.level == len(list_of_items) -1:
+            continue
+
+        v.level = u.level + 1
+        v.weight = u.weight + list_of_items[v.level].weight;
+        v.profit = u.profit + list_of_items[v.level].value;
+
+        # If cumulated weight is less than W and profit is greater than previous profit, update max_profit.
+        if v.weight <= total_knapsack_weight and v.profit > max_profit:
+            max_profit = v.profit
+
+        v.bound = bound(v, total_knapsack_weight, list_of_items)
+        if v.bound > max_profit:
+            current_queue.append(v)
+
+        # Do the same thing, but without taking the item in knapsack.
+        v.weight = u.weight;
+        v.profit = u.profit;
+        v.bound = bound(v, total_knapsack_weight, list_of_items);
+        if v.bound > maxProfit:
+            current_queue.append(v)
+
+    return max_profit
+
+
+if __name__ == "__main__":
+    item_values = [60, 100, 120, 2]
+    wt = [10, 20, 29, 1]
+    W = 50
+    list_of_items = []
+    item_value_pairs = [(2, 40), (3.14, 50), (1.98, 100), (5, 95), (3, 30)]
+    print(
+        knapsack_bb(
+            total_knapsack_weight=W, 
+            item_value_pairs=item_value_pairs,
+        )
+    )
