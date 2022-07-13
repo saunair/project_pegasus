2c2feca0ba01c6f897f6c98dd1e3c46e55d1e569
Saurabh Nair <saurabh.nk@gmail.com>
2022-07-13 07:46:10 -0700

    closest point works

--- leetcode/array/closest_points/closest_point.cxx
+++ leetcode/array/closest_points/closest_point.cxx
@@ -2,48 +2,73 @@
 #include<vector>
 #include<math.h>
 #include <algorithm>
+#include<cctype>
+#include<climits>
 
 using namespace std;
 
 
-vector<vector<int>> k_closest_points(vector<vector<int>> all_points, vector<int> point, int k){
+class Point{
+    public:
+        int x = -INT_MIN; 
+        int y = -INT_MAX;
+        Point(int x_coordinate, int y_coordinate);
+        Point();
+};
+
+
+Point::Point(){
+    return;
+}
+
+Point::Point(int x_coordinate, int y_coordinate){
+    x = x_coordinate;
+    y = y_coordinate;
+}
+
+ostream &operator<<(ostream &os, Point const &m) {
+    return os << "["<<m.x <<"\t"<<m.y<<"]"<<endl;
+}
+
+vector<Point> k_closest_points(vector<Point> all_points, Point point, int k){
    int num_points = all_points.size();
    vector<int> distances(num_points, -1);
    int point_count = 0;
-   for(vector<vector<int>>::iterator it=all_points.begin(); it!=all_points.end(); ++it){
-       distances[point_count] = sqrt((point[0] - (*it)[0])*(point[0] - (*it)[0]) + (point[1] - (*it)[1])*(point[1] - (*it)[1]));
+   for(auto it:all_points){
+       distances[point_count] = sqrt((point.x - it.x)*(point.x - it.x) + (point.y - it.y)*(point.y - it.y));
        point_count++;
    }
    
    int k_count = k;
    int index_num = 0;
-   vector<vector<int>> k_points(k);
+   vector<Point> k_points(k);
    for(int k_count=k; k_count>0; k_count--){
        auto it_max = min_element(distances.begin(), distances.end());
        index_num = distance(distances.begin(), it_max);
-       k_points[index_num] = all_points[index_num];
+       k_points[k - k_count] = all_points[index_num];
+       //Remove the point entries associated to these.
        distances.erase(it_max);
        all_points.erase(all_points.begin() + index_num);
+       cout<<k_points[k - k_count]<<"out"<<index_num - k<<endl;
    }
    return k_points;
 }
 
 
-void display_points(vector<vector<int>> points){
+void display_points(vector<Point> points){
     int count = 0;
-
-    for(vector<vector<int>>::iterator it=points.begin(); it!=points.end(); ++it){
+    for(auto it:points){
         cout<<count<<endl;
-        cout<<"point: ["<<(*it)[0]<<"\t"<<(*it)[1]<<"]"<<endl;
+        cout<<"point: "<<it;
         count++;
     }
 }
 
 
 int main(int argc, const char* argv){
-    vector<vector<int>> point_array1 = {{3,3}, {5,-1}, {-2,4}};
-    vector<int> point(2, 0);
-    vector<vector<int>> k = k_closest_points(point_array1, point, 3);
+    vector<Point> point_array1 = {Point(3,3), Point(5,-1), Point(-2,4)};
+    Point point(0, 0);
+    vector<Point> k = k_closest_points(point_array1, point, 3);
     display_points(k);
     return 0;
 }
